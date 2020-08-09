b5a30155a79c03f71dbe6f03e33e197af8b2666e
Ibrahim Settar <hisetkal@gmail.com>
2020-08-09 11:12:14 -0400

    the second commit from local repo

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the first changes from local master");
-		
+		System.out.println("This is the second commit from local master");
 		
 	}
 }
